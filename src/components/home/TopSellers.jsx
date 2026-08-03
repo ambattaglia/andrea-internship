efed28aadb39f34ec724dd94ae03c667e09443f0
ambattaglia <am.battaglia@yahoo.com>
2026-08-03 14:38:55 -0400

    Top Sellers, API, Skelton Loading State

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,40 +1,112 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // API FETCH + REORDER
+  useEffect(() => {
+    axios
+      .get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+      )
+      .then((res) => {
+        const selected = localStorage.getItem("selectedAuthor");
+        let data = res.data;
+
+        if (selected) {
+          data = [
+            ...data.filter((s) => s.authorId === selected),
+            ...data.filter((s) => s.authorId !== selected)
+          ];
+        }
+
+        setSellers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
         <div className="row">
+
+          {/* TITLE */}
           <div className="col-lg-12">
             <div className="text-center">
               <h2>Top Sellers</h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
+
+          {/* LIST */}
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+
+              {/* SKELETON */}
+              {loading &&
+                new Array(12).fill(0).map((_, index) => (
+                  <li key={index}>
+                    <div
+                      className="author_list_pp skeleton-box"
+                      style={{
+                        width: "50px",
+                        height: "50px",
+                        borderRadius: "50%"
+                      }}
+                    ></div>
+
+                    <div className="author_list_info">
+                      <div
+                        className="skeleton-box"
+                        style={{ width: "120px", height: "20px", marginBottom: "6px" }}
+                      ></div>
+                      <div
+                        className="skeleton-box"
+                        style={{ width: "60px", height: "15px" }}
+                      ></div>
+                    </div>
+                  </li>
+                ))}
+
+              {/* REAL DATA */}
+              {!loading &&
+                sellers.map((seller) => (
+                  <li key={seller.id}>
+                    <div className="author_list_pp">
+                      <Link
+                        to={`/author/${seller.authorId}`}
+                        onClick={() =>
+                          localStorage.setItem("selectedAuthor", seller.authorId)
+                        }
+                      >
+                        <img
+                          className="lazy pp-author"
+                          src={seller.authorImage}
+                          alt={seller.name}
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+
+                    <div className="author_list_info">
+                      <Link to={`/author/${seller.authorId}`}>
+                        {seller.name}
+                      </Link>
+                      <span>{seller.price} ETH</span>
+                    </div>
+                  </li>
+                ))}
+
             </ol>
           </div>
+
         </div>
       </div>
     </section>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { Link, useParams } from "react-router-dom";
 
 const Author = () => {
+  const { id } = useParams();
+  const [author, setAuthor] = useState(null);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Scroll to top when page loads
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  // Fetch author data
+  useEffect(() => {
+    axios
+      .get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`
+      )
+      .then((res) => {
+        setAuthor(res.data);
+        setItems(res.data.nftCollection || []);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, [id]);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -21,43 +48,102 @@ const Author = () => {
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              {/* PROFILE HEADER */}
+<div className="col-md-12">
+  {loading ? (
+    // SKELETON HEADER
+    <div className="d_profile de-flex">
+      <div className="de-flex-col">
+        <div className="profile_avatar">
+          <div
+            className="skeleton-box"
+            style={{
+              width: "120px",
+              height: "120px",
+              borderRadius: "50%",
+            }}
+          ></div>
 
+          <div className="profile_name" style={{ marginTop: "20px" }}>
+            <div
+              className="skeleton-box"
+              style={{ width: "200px", height: "25px", marginBottom: "10px" }}
+            ></div>
+            <div
+              className="skeleton-box"
+              style={{ width: "150px", height: "20px", marginBottom: "10px" }}
+            ></div>
+            <div
+              className="skeleton-box"
+              style={{ width: "250px", height: "20px" }}
+            ></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="profile_follow de-flex">
+        <div className="de-flex-col">
+          <div
+            className="skeleton-box"
+            style={{ width: "120px", height: "20px", marginBottom: "10px" }}
+          ></div>
+          <div
+            className="skeleton-box"
+            style={{ width: "100px", height: "35px" }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    // REAL HEADER
+    <div className="d_profile de-flex">
+      <div className="de-flex-col">
+        <div className="profile_avatar">
+          <img src={author?.authorImage} alt={author?.authorName} />
+          <i className="fa fa-check"></i>
+
+          <div className="profile_name">
+            <h4>
+              {author?.authorName}
+              <span className="profile_username">@{author?.tag}</span>
+              <span id="wallet" className="profile_wallet">
+                {author?.address}
+              </span>
+              <button id="btn_copy" title="Copy Text">
+                Copy
+              </button>
+            </h4>
+          </div>
+        </div>
+      </div>
+
+      <div className="profile_follow de-flex">
+        <div className="de-flex-col">
+          <div className="profile_follower">
+            {author?.followers} followers
+          </div>
+          <Link to="#" className="btn-main">
+            Follow
+          </Link>
+        </div>
+      </div>
+    </div>
+  )}
+</div>
+
+
+              {/* AUTHOR ITEMS */}
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems
+                    items={items}
+                    loading={loading}
+                    author={author}
+                  />
                 </div>
               </div>
+
             </div>
           </div>
         </section>
