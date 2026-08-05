7caca3a2233b6e2c7e250c7d9490af5978106a5c
ambattaglia <am.battaglia@yahoo.com>
2026-08-05 14:46:30 -0400

    Remove unused Link import in Author page to fix Vercel CI build warning

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import AuthorItems from "../components/author/AuthorItems";
 import AuthorBanner from "../images/author_banner.jpg";
 
